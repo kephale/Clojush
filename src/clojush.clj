1e3670e2ed9056f7b02e2ff13bb61ea1ab685e4a
Kyle Harrington <kyleh@cs.brandeis.edu>
2011-11-04 07:41:47 -0400

    Pareto-based decimation.

--- src/clojush.clj
+++ src/clojush.clj
@@ -2041,8 +2041,46 @@ elimination tournaments to reach the provided target-size."
                      tournament-index-set)]
                (vec (concat (subvec population 0 victim-index)
                       (subvec population (inc victim-index)))))
-        target-size tournament-size radius))))
+             target-size tournament-size radius))))
 
+(defn pareto-tournament
+  "Return the index of an individual that lost a pareto tournament, nil if there is no loser."
+  [population radius]  
+  (let [loc1 (lrand-int (count population))
+        loc2 (if (zero? radius)
+               (lrand-int (count population))
+               (mod (+ loc1 (- (lrand-int (+ 1 (* radius 2))) radius))
+                    (count population)))
+        errs1 (:errors (nth population loc1))
+        errs2 (:errors (nth population loc2))
+        loc1-dominates? (and (reduce #(and %1 %2) (map <= errs1 errs2))
+                             (reduce #(or %1 %2) (map < errs1 errs2)))
+        loc2-dominates? (and (reduce #(and %1 %2) (map <= errs2 errs1))
+                             (reduce #(or %1 %2) (map < errs2 errs1)))]
+    (cond loc1-dominates? loc1
+          loc2-dominates? loc2
+          :else nil)))
+
+(defn pareto-decimate
+  "Returns the subset of the provided population remaining after sufficiently many
+elimination tournaments to reach near provided target-size. This is approximate
+because there is a possibility that even the entire population is non-dominated."
+  [population target-size tournament-size radius]
+  (loop [population (vec population)
+         attempts 0]
+    (if (or (<= (count population) target-size)
+            (> attempts 5))
+      population
+      (let [victims (keep identity
+                          (apply pcalls (repeat (- (count population) target-size)
+                                                #(pareto-tournament population radius))))]
+        (if (empty? victims)
+          (recur population (inc attempts))
+          (recur (vec (keep identity (apply assoc
+                                            (cons population                                         
+                                                  (interleave victims
+                                                              (repeat (count victims) nil)))))) 0))))))
+  
 (defn scaled-errors
   "A utility function for use in error functions, to implement error-scaling as described
 by Maarten Keijzer in Scaled Symbolic Regression, in Genetic Programming and Evolvable
@@ -2183,7 +2221,7 @@ example."
           (do (if (>= generation max-generations)
                 (printf "\nFAILURE\n")
                 (do (printf "\nProducing offspring...") (flush)
-                  (let [pop (decimate (vec (doall (map deref pop-agents))) 
+                  (let [pop (pareto-decimate (vec (doall (map deref pop-agents))) 
                               (int (* decimation-ratio population-size))
                               decimation-tournament-size 
                               trivial-geography-radius)]
